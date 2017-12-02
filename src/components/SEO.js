f221b61bbb30fab43201c6a0763be9ad7b921277
OGURA_Daiki <8hachibee125@gmail.com>
2017-12-02 12:45:47 +0900

    adjust some style

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -25,21 +25,21 @@ export default function SEO({ postNode, postPath, postSEO }) {
   const blogURL = config.siteUrl + config.pathPrefix;
   const schemaOrgJSONLD = [
     {
-      "@context": "http://schema.org",
-      "@type": "WebSite",
+      '@context': 'http://schema.org',
+      '@type': 'WebSite',
       url: blogURL,
       name: title,
-      alternateName: config.siteTitleAlt ? config.siteTitleAlt : ""
+      alternateName: config.siteTitleAlt ? config.siteTitleAlt : ''
     }
   ];
   if (postSEO) {
     schemaOrgJSONLD.push([
       {
-        "@context": "http://schema.org",
-        "@type": "BreadcrumbList",
+        '@context': 'http://schema.org',
+        '@type': 'BreadcrumbList',
         itemListElement: [
           {
-            "@type": "ListItem",
+            '@type': 'ListItem',
             position: 1,
             item: {
               "@id": postURL,
@@ -50,14 +50,14 @@ export default function SEO({ postNode, postPath, postSEO }) {
         ]
       },
       {
-        "@context": "http://schema.org",
-        "@type": "BlogPosting",
+        '@context': 'http://schema.org',
+        '@type': 'BlogPosting',
         url: blogURL,
         name: title,
-        alternateName: config.siteTitleAlt ? config.siteTitleAlt : "",
+        alternateName: config.siteTitleAlt ? config.siteTitleAlt : '',
         headline: title,
         image: {
-          "@type": "ImageObject",
+          '@type': 'ImageObject',
           url: image
         },
         description
@@ -90,7 +90,7 @@ export default function SEO({ postNode, postPath, postSEO }) {
       <meta name="twitter:card" content="summary_large_image" />
       <meta
         name="twitter:creator"
-        content={config.userTwitter ? config.userTwitter : ""}
+        content={config.userTwitter ? config.userTwitter : ''}
       />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
